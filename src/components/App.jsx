5b42d74664ec2aae49571e2faea405ea92f0fcbc
Igorippon <botvunaigor@gmail.com>
2023-09-09 10:54:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,17 @@ import { Filter } from "./Filter/Filter";
 import { ContactForm } from "./ContactForm/ContactForm.js";
 import { ContactList } from "./ContactList/ContactList.js";
 
+const getIntialContacts = () => {
+  return JSON.parse(localStorage.getItem("contacts")) ?? [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+};
+
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem("contacts")) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  });
+  const [contacts, setContacts] = useState(getIntialContacts);
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
